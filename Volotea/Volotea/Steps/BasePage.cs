646729071743d8890bef38401e873d621af22d5c
nrusinovich <nadyarusinovich@gmail.com>
2017-03-22 10:08:15 +0300

    Update BasePage.cs

--- Volotea/Volotea/Steps/BasePage.cs
+++ Volotea/Volotea/Steps/BasePage.cs
@@ -11,7 +11,7 @@ namespace Volotea.Steps
 {
     public class BasePage
     {
-        protected IWebDriver Driver { get; set; }
+        public IWebDriver Driver { get; set; }
 
         public BasePage(string browser)
         {
